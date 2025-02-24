54ab08a9eb837f363def44add92d2e474ab75dbd
Thomas Schorn <t@schorn.io>
2025-02-24 11:08:28 +0100

    feature: unstable: position and resize handle

--- src/examples/MarkdownTest.jsx
+++ src/examples/MarkdownTest.jsx
@@ -5,6 +5,7 @@ import {
   FontFamilyProvider,
   DefaultProperties,
 } from "@react-three/uikit";
+import { Handle, HandleTarget } from "@react-three/handle";
 import { PointerEvents, XR } from "@react-three/xr";
 import React from "react";
 import ScrollableContainer from "@/src/component/ScrollableContainer";
@@ -18,45 +19,55 @@ export default function App() {
       gl={{ localClippingEnabled: true }}
     >
       <XR store={xrStore}>
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight
+          position={[10, 10, 10]}
+          angle={0.15}
+          penumbra={1}
+          decay={0}
+          intensity={Math.PI}
+        />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
         <PointerEvents />
-        <Root
-          backgroundColor="#42258d"
-          sizeX={8}
-          sizeY={6}
-          flexDirection="row"
-          borderRadius={10}
-          padding={10}
-          gap={10}
-        >
-          <FontFamilyProvider
-            inter={{
-              normal: "inter-normal.json",
-              bold: "Inter_18pt-Bold-msdf.json",
-              "semi-bold": "Inter_18pt-SemiBold-msdf.json",
-            }}
-            italic={{
-              normal: "Inter_18pt-Italic-msdf.json",
-              bold: "Inter_18pt-BoldItalic-msdf.json",
-            }}
+        <HandleTarget>
+          <Root
+            backgroundColor="#42258d"
+            sizeX={8}
+            sizeY={6}
+            flexDirection="row"
+            borderRadius={10}
+            padding={10}
+            gap={10}
           >
-            <DefaultProperties fontFamily="inter">
-              <Container
-                flexGrow={1}
-                backgroundColor="#9a398d"
-                flexDirection="row"
-                padding={10}
-                gap={10}
-              >
-                <ScrollableContainer
-                  minWidth="10%"
-                  backgroundColor="white"
-                  backgroundOpacity={0.5}
-                  padding={10}
+            <FontFamilyProvider
+              inter={{
+                normal: "inter-normal.json",
+                bold: "Inter_18pt-Bold-msdf.json",
+                "semi-bold": "Inter_18pt-SemiBold-msdf.json",
+              }}
+              italic={{
+                normal: "Inter_18pt-Italic-msdf.json",
+                bold: "Inter_18pt-BoldItalic-msdf.json",
+              }}
+            >
+              <DefaultProperties fontFamily="inter">
+                <Container
+                  flexGrow={1}
+                  backgroundColor="#9a398d"
                   flexDirection="row"
-                  scrollToEnd={false}
+                  padding={10}
+                  gap={10}
                 >
-                  <MarkdownParser>
-                    {`# Überschrift _italic text_
+                  <ScrollableContainer
+                    minWidth="10%"
+                    backgroundColor="white"
+                    backgroundOpacity={0.5}
+                    padding={10}
+                    flexDirection="row"
+                    scrollToEnd={false}
+                  >
+                    <MarkdownParser>
+                      {`# Überschrift _italic text_
                     ## Unterüberschrift
                     Normaler Text hier. I just love **bold text**. Und noch einmal _italic + **bold** text_. Und das ganze nochmal ***gleichzeitig***.
                     
@@ -77,12 +88,31 @@ export default function App() {
                     
                     > Zitat
                     > line 2 with **bold text**`}
-                  </MarkdownParser>
-                </ScrollableContainer>
-              </Container>
-            </DefaultProperties>
-          </FontFamilyProvider>
-        </Root>
+                    </MarkdownParser>
+                  </ScrollableContainer>
+                </Container>
+              </DefaultProperties>
+            </FontFamilyProvider>
+          </Root>
+          <Handle targetRef="from-context" rotate={false} scale={false}>
+            <mesh position={[0, -3, 0]}>
+              <boxGeometry args={[3, 0.2, 0.2]} />
+              <meshStandardMaterial color="hotpink" />
+            </mesh>
+          </Handle>
+          <Handle
+            targetRef="from-context"
+            scale={{ uniform: true }}
+            multitouch={false}
+            translate="as-scale"
+            rotate={false}
+          >
+            <mesh position={[4, -3, 0]}>
+              <boxGeometry args={[0.2, 0.2, 0.2]} />
+              <meshStandardMaterial color="purple" />
+            </mesh>
+          </Handle>
+        </HandleTarget>
       </XR>
     </Canvas>
   );
